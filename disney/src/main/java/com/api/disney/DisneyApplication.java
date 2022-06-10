7da398b634e6cb0eb0a0bd36e65766bb561ec3a4
Maxi <maxifop96@gmail.com>
2022-06-10 11:56:05 -0300

    Build project

---
+++ disney/src/main/java/com/api/disney/DisneyApplication.java
@@ -0,0 +1,13 @@
+package com.api.disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyApplication.class, args);
+	}
+
+}
