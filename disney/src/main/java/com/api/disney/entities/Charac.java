f6602514f4b6bf16894c8e7dd81899784e2e7cdc
Maxi Falcone <maxifop96@gmail.com>
2022-06-11 20:39:48 -0300

    Entities filled

--- disney/src/main/java/com/api/disney/entities/Charac.java
+++ disney/src/main/java/com/api/disney/entities/Charac.java
@@ -1,9 +1,12 @@
 package com.api.disney.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
@@ -19,11 +22,15 @@ public class Charac {
     @Column
     private String picture;
     @Column(nullable = false, length = 100)
+    @NotNull(message = "The name can not be null.")
+    @NotEmpty(message = "The name can not be empty.")
     private String name;
     @Column(nullable = false)
+    @NotNull(message = "The age can not be null.")
+    @NotEmpty(message = "The age can not be empty.")
     private Integer age;
-    @Column(nullable = false)
-    private Double weight;
+    @Column
+    private Integer weight;
     @Column(nullable = false)
     private String story;
     @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
@@ -31,6 +38,7 @@ public class Charac {
             , joinColumns = @JoinColumn(name="character_id")
             , inverseJoinColumns = @JoinColumn(name = "movie_id"))
     @ToString.Exclude
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<Movie> movies;
 
 }
